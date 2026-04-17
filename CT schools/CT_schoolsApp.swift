3903a2448cbd96ccd90e792f30133be0769b94e7
Neikkie <neikkie21@outlook.com>
2026-04-16 23:40:13 -0400

    Initial Commit

---
+++ CT schools/CT_schoolsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CT_schoolsApp.swift
+//  CT schools
+//
+//  Created by Shanique Beckford on 4/16/26.
+//
+
+import SwiftUI
+
+@main
+struct CT_schoolsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
